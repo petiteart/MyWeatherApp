74fbb3cf3b4b3b5495e0a1bcb4a0ffe73a7b6bd3
PetiteArt <pawita1993@gmail.com>
2018-12-02 18:02:51 +0000

    my second commit

--- app/src/main/java/com/example/pawita/myweatherapp/MainActivity.java
+++ app/src/main/java/com/example/pawita/myweatherapp/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.pawita.myweatherapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = findViewById(R.id.display);
+    }
+
+    public void displayText(View view) {
+        if (text != null) {
+            if (text.getText().length() == 0) {
+                text.setText("Pawita es guapa");
+            } else {
+                text.setText("");
+            }
+        }
     }
 }
